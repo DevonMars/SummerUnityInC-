e8f7599f7b875f0ad2dace0f8d21a403b772a720
Unknown <devon_marsham_16@live.nl>
2017-07-06 03:53:36 +0200

    NumberWizard

---
+++ Unitiy/NumberWizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Welkom to NumberWIzard");
+		print ("Pick a number, but don't tell me!!");
+		int numMax = 1000;
+		int numMin = 1;
+		
+		print ("The highest number you can pick is "  + numMax + ", while the lowest number you can pick is "  + numMin);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
